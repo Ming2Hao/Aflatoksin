ff69ac6dd7a90526d808101454c7991a63aa982b
Ming2Hao <ivancs2010gg@gmail.com>
2025-03-03 16:25:45 +0000

    fixed camera capture (without API)

--- controlCamera/main.py
+++ controlCamera/main.py
@@ -4,11 +4,10 @@ from sh import gphoto2 as gp
 import signal, os, subprocess
 
 shot_date = datetime.now().strftime("%Y-%m-%d")
-shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+shot_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 picID = "PiShots_" + shot_time
 
-captureAndDownloadCommand = ["--capture-image-and-download"]
-changeFileNameCommand = ["--filename "+picID+".jpg"]
+captureAndDownloadCommand = ["--capture-image-and-download","--filename",picID+".jpg"]
 
 folder_name = shot_date + picID
 
@@ -21,7 +20,6 @@ def createSaveFolder():
     print("Changed to directory: " + folder_name)
 
 def captureImages():
-    gp(changeFileNameCommand)
     gp(captureAndDownloadCommand)
     print("Captured the image: "+picID+".jpg")
 
